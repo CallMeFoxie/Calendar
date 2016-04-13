88382aed827b5787c6d79a439df9b89106ef50e9
Ashley <github@victorianfox.com>
2016-04-13 21:46:12 +0200

    Add local version helper

---
+++ src/main/java/foxie/calendar/MCLocalVersionHelper.java
@@ -0,0 +1,25 @@
+package foxie.calendar;
+
+import net.minecraft.util.StatCollector;
+
+public class MCLocalVersionHelper {
+   public static String translateToLocal(String key) {
+      return StatCollector.translateToLocal(key);
+   }
+
+   public static String translateToLocalFormatted(String key, Object... format) {
+      return StatCollector.translateToLocalFormatted(key, format);
+   }
+
+   public static String translateToFallback(String key) {
+      return StatCollector.translateToFallback(key);
+   }
+
+   public static boolean canTranslate(String key) {
+      return StatCollector.canTranslate(key);
+   }
+
+   public static long getLastTranslationUpdateTimeInMilliseconds() {
+      return StatCollector.getLastTranslationUpdateTimeInMilliseconds();
+   }
+}
